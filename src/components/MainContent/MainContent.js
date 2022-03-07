e26ab417feebac1f3d5038d57413c4b11b4ce53f
leolicona <leolicona.dev@gmail.com>
2022-03-07 10:28:56 -0600

    MainContent and styling

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,7 +1,17 @@
 import React from "react"
+import "./styles.css/main.css"
 
 export default function MainContent() {
     return(
-        <h1>Hey, I am the MainContent component</h1>
+        <main className=".main">
+        <h2 className="main--title">Fun facts about React</h2>
+        <ul className="main--list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
