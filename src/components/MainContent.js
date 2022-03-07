f3c5b5a8b0f14f675ade9f1433d6576242758cb9
leolicona <leolicona.dev@gmail.com>
2022-03-07 08:15:26 -0600

    index.js and App.js files was created

---
+++ src/components/MainContent.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function MainContent() {
+    return(
+        <h1>Hey, I am the MainContent component</h1>
+    )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function NavBar () {
+    return(
+        <h1>Hey, I am the NavBar</h1>
+    )
+}
